64351d539637f0e8a4d97441e45bd59099ae4bf7
KingCharlesVI <89871965+KingCharlesVI@users.noreply.github.com>
2023-04-27 21:57:29 +0100

    Adding sidebar

--- fps-website/src/App.vue
+++ fps-website/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import sidebar from './components/sidebar/sidebar.vue'
 </script>
 
 <template>
@@ -8,12 +9,12 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Coming Soon!" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <sidebar />
   </main>
 </template>
 
@@ -31,17 +32,14 @@ header {
   header {
     display: flex;
     place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
   }
 
   .logo {
     margin: 0 2rem 0 0;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .wrapper {
+    margin: auto;
   }
 }
 </style>
